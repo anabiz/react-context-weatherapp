ca76da6c77d71aebc2cf29a746bbdca882833bdd
anabiz <anabizconcept9@gmail.com>
2021-01-23 14:13:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, {createContext, useState, useContext} from 'react';
+import CityList from "./CityList";
+import AddCityButton from "./AddCityButton";
+import TemperatureAverage from "./TemperatureAverage";
 import './App.css';
 
+export const WeatherContext = createContext();
+
 function App() {
+  const [cities, setCities] = useState([]);
+
+  const addCity = (name, temperature) => {
+    const newCity = { name, temperature };
+    setCities(prevCities => [...prevCities, { name, temperature }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WeatherContext.Provider value={{
+      cities,
+      addCity,
+    }}>
+      <div className="city-overview">
+        <h2>Multi-Weather App</h2>
+        <CityList />
+        <AddCityButton />
+        <TemperatureAverage />
+      </div>
+    </WeatherContext.Provider>
   );
 }
 
